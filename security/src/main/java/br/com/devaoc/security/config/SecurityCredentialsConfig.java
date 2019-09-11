3018c87c0a31f9798ff7ee166277e9575d9d28a7
afonsoaoc <afonso.aoc@gmail.com>
2019-09-10 22:53:01 -0300

    separação da criação do token e ajustes no projeto security.

--- security/src/main/java/br/com/devaoc/security/config/SecurityCredentialsConfig.java
+++ security/src/main/java/br/com/devaoc/security/config/SecurityCredentialsConfig.java
@@ -2,46 +2,36 @@ package br.com.devaoc.security.config;
 
 import br.com.devaoc.core.property.JwtConfiguration;
 import br.com.devaoc.security.filter.JwtUserNameAndPasswordAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.devaoc.security.token.creator.TokenCreator;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-
-import javax.servlet.http.HttpServletResponse;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
 @EnableWebSecurity
-@RequiredArgsConstructor(onConstructor =  @__(@Autowired))
-public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
+public class SecurityCredentialsConfig extends SecurityTokenConfig {
 
     private final UserDetailsService userDetailsService;
-    private final JwtConfiguration jwtConfiguration;
+    private final TokenCreator tokenCreator;
+
+    public SecurityCredentialsConfig(JwtConfiguration jwtConfiguration,
+                                    @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService,
+                                     TokenCreator tokenCreator) {
+
+        super(jwtConfiguration);
+        this.userDetailsService = userDetailsService;
+        this.tokenCreator = tokenCreator;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
-                .and()
-                    .sessionManagement().sessionCreationPolicy(STATELESS)
-                .and()
-                    .exceptionHandling().authenticationEntryPoint((req, resp, e) -> resp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-                .and()
-                    .addFilter(new JwtUserNameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfiguration))
-                .authorizeRequests()
-                    .antMatchers(jwtConfiguration.getLoginUrl()).permitAll()
-                    .antMatchers("/api-example/admin/**").hasRole("ADMIN")
-                    .anyRequest().authenticated();
-        }
+                .addFilter(new JwtUserNameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfiguration, tokenCreator));
+        super.configure(http);
+    }
 
     // metodo que faz autenticacao chamando findByUserName
     @Override
