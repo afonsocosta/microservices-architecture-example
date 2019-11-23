873188038f9e26068975b87818db898e12315cb7
afonsoaoc <afonso.aoc@gmail.com>
2019-11-23 13:15:31 -0300

    criado filtro para autorização de todos os micro serviços

--- security/src/main/java/br/com/devaoc/security/config/SecurityCredentialsConfig.java
+++ security/src/main/java/br/com/devaoc/security/config/SecurityCredentialsConfig.java
@@ -1,7 +1,9 @@
 package br.com.devaoc.security.config;
 
 import br.com.devaoc.core.property.JwtConfiguration;
+import br.com.devaoc.security.filter.JwtTokenAuthorizationFilter;
 import br.com.devaoc.security.filter.JwtUserNameAndPasswordAuthenticationFilter;
+import br.com.devaoc.security.token.converter.TokenConverter;
 import br.com.devaoc.security.token.creator.TokenCreator;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -10,26 +12,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityCredentialsConfig extends SecurityTokenConfig {
 
     private final UserDetailsService userDetailsService;
     private final TokenCreator tokenCreator;
+    private final TokenConverter tokenConverter;
 
     public SecurityCredentialsConfig(JwtConfiguration jwtConfiguration,
-                                    @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService,
-                                     TokenCreator tokenCreator) {
+                                     @Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService,
+                                     TokenCreator tokenCreator, TokenConverter tokenConverter) {
 
         super(jwtConfiguration);
         this.userDetailsService = userDetailsService;
         this.tokenCreator = tokenCreator;
+        this.tokenConverter = tokenConverter;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .addFilter(new JwtUserNameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfiguration, tokenCreator));
+                .addFilter(new JwtUserNameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfiguration, tokenCreator))
+                .addFilterAfter(new JwtTokenAuthorizationFilter(jwtConfiguration, tokenConverter), UsernamePasswordAuthenticationFilter.class);
         super.configure(http);
     }
 
